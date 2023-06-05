573efd31a18e353604c095feb30009194cbfbd2a
koyotex <david242455@gmail.com>
2023-06-05 07:59:46 -0400

    change port to 80

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const { token } = require('./middleware/session')
 const { router } = require('./routes')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 80
 
 app.use(cors())
 app.use('/', token, router)
